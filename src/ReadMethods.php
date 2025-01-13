f2edbd855e12e47d47dedf341b40e16a4f61bbd1
TOBENTOch <info@tobento.ch>
2025-01-13 18:27:23 +0100

    findColumn implemented

--- src/ReadMethods.php
+++ src/ReadMethods.php
@@ -101,6 +101,31 @@ trait ReadMethods
     }
     
     /**
+     * Returns the found column values using the specified parameters.
+     *
+     * @param string $column The column name for the values.
+     * @param null|string $key The column name for the index key.
+     * @param array $where Usually where parameters.
+     * @param array $orderBy The order by parameters.
+     * @param null|int|array $limit The limit e.g. 5 or [5(number), 10(offset)].
+     * @return array
+     * @throws RepositoryReadException
+     */
+    public function findColumn(
+        string $column,
+        null|string $key = null,
+        array $where = [],
+        array $orderBy = [],
+        null|int|array $limit = null
+    ): array {
+        $query = $this->query();
+        $query = $this->applyWhere($query, $where);
+        $query = $this->applyOrderBy($query, $orderBy);
+        $query = $this->applyLimit($query, $limit);
+        return $query->column($column, $key)->all();
+    }
+    
+    /**
      * Returns the number of items using the specified where parameters.
      *
      * @param array $where
